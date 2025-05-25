fd96f2dac3ab6774123006e45c30b4bbac06c34a
drguandinango <drguandinango@pucesi.edu.ec>
2025-05-24 22:19:53 -0500

    AE-71 se realiza la subida debackedn a NEST DESDE PYTHON Actualizando configuración de base de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,3 +20,13 @@ import { AuthModule } from './auth/auth.module';
   ],
 })
 export class AppModule {}
+/*type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'login_app',
+      autoLoadEntities: true,
+      synchronize: true,
+
+      */
