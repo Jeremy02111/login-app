960c379ca104c476523af0e28c3e06b8262eb750
drguandinango <drguandinango@pucesi.edu.ec>
2025-07-08 13:00:46 -0500

    AE-166 Mostrar tabla jerárquica en el frontend

--- src/app.module.ts
+++ src/app.module.ts
@@ -22,4 +22,4 @@ import { TrabajadoresModule } from './trabajadores/trabajadores.module';
   ],
 })
 export class AppModule {}
-//todo está subido en el backedn en la ramam de jeremy
+//todo está subido en el backedn en la ramam de jeremyx |
--- src/trabajadores/trabajadores.controller.ts
+++ src/trabajadores/trabajadores.controller.ts
@@ -2,10 +2,16 @@ import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 import { TrabajadoresService } from './trabajadores.service';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { RolesGuard } from '../auth/roles.guard';
+import { Role } from '../auth/role.decorator';
 
 @ApiTags('trabajadores')
 @ApiBearerAuth()
 @UseGuards(JwtAuthGuard)
+
+@UseGuards(JwtAuthGuard, RolesGuard)
+@Role('admin')
+
 @Controller('trabajadores')
 export class TrabajadoresController {
   constructor(private readonly trabajadoresService: TrabajadoresService) {}
